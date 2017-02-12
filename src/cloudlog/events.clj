2e00408cfd08b5e14cbad0bc63788d2b7381ee85
Boaz Rosenan <brosenan@gmail.com>
2017-02-12 20:21:40 +0200

    emitter

--- src/cloudlog/events.clj
+++ src/cloudlog/events.clj
@@ -1,1 +1,14 @@
-(ns cloudlog.events)
+(ns cloudlog.events
+  (:use [cloudlog.core]))
+
+(defn emitter [rulefunc]
+  (fn [event]
+    (for [data (rulefunc (cons (:key event) (:data event)))]
+      (merge
+       (if (-> rulefunc meta :target-fact)
+                    (merge event {:name (-> rulefunc meta :target-fact fact-table)})
+                                        ; else
+                    (merge event {:kind :rule
+                                  :name (str (fact-table [rulefunc]) "!0")}))
+       {:key (first data)
+        :data (rest data)}))))
--- test/cloudlog/events_test.clj
+++ test/cloudlog/events_test.clj
@@ -17,10 +17,69 @@ on the internal state of a rule. Each event contains the following fields:
 - `:kind`: Either `:fact`, if this event represent a change to a fact, or `:rule` if it represents a change to a rule.
 - `:name`: A string representing the name of the stream this event belongs to.  See [fact-table](core.html#fact-table-get-a-fully-qualified-name-for-a-fact) for details.
 - `:key`: The key of the fact or the rule.  See [here](core.html#joins) for more details.
+- `:ts`: The time (in milliseconds since EPOCH) in which this event was created (for facts.  See below for rules).
 - `:data`: The data tuple representing a fact, or the state of a rule, excluding the key.
 - `:change`: A number representing the change.  Typically, `1` represents addition, and `-1` represents removal.
 - `:writers`: The event's *writer-set*, represented as an [interset](interset.html).
 - `:readers`: The event's *reader-set*, represented as an [interset](interset.html)."
+
+"For the examples of this package we will use the following convenience function:"
+
+(defn event [kind name key data & {:keys [ts change writers readers] :or {ts 1000
+                                                                          change 1
+                                                                          writers #{}
+                                                                          readers #{}}}]
+  {:kind kind
+   :name name
+   :key key
+   :data data
+   :ts ts
+   :change change
+   :writers writers
+   :readers readers})
+
+(fact
+ (event :fact "foo" 13 ["a" "b"] :ts 2000) => {:kind :fact
+                                               :name "foo"
+                                               :key 13
+                                               :data ["a" "b"]
+                                               :ts 2000
+                                               :change 1
+                                               :writers #{}
+                                               :readers #{}})
+
+[[:chapter {:title "emitter: Create an Event-Emitting Function"}]]
+"Rules start by matching a single fact.  An emitter function takes an event
+representing such a fact and applies the rule function associated with the event."
+
+"For a simple rule, the result is a sequence of derived fact."
+(fact
+ (let [em (emitter foo-yx)]
+   (em (event :fact "test/foo" 2 [3]))
+   => [(event :fact "cloudlog.core_test/foo-yx" 3 [2])]))
+
+"For a join, the result is an event representing the *rule* produced from the fact.
+
+Here some explanation is in order.  Our notion of facts and rules come from mathematical logic.
+Facts are what mathematical logic calls *atoms* -- a combination of a name with some arguments,
+and rules are logic formulas of the form `a->b`, where the `->` operator represents logical inference.
+In the subset of mathematical logic we adopted, the left-hand-side of the `->` operator must be an atom
+(a fact).  However, the right-hand side can be any kind of axiom -- fact or rule.  With this
+we can create compound rules such as `a->b->c->d`, that should be read as `a->(b->(c->d))`.
+This means that the fact `a` implies the rule `b->c->d`, which in turn means that
+the fact `b` implies `c->d`, which in turn means that `c` implies `d`.
+In cloudlog.clj, rule functions take whatever matches the left-hand side of the rule, and
+emit whatever is on the right-hand side, be it a (derived) fact or a rule.
+Our implementation does not emit the rule syntactically.  Instead it provides a tuple
+that contains its underlying data.  But we still treat it as a rule."
+
+(fact
+ (let [em (emitter timeline)]
+   (em (event :fact "test/follows" "alice" ["bob"]))
+   => [(event :rule "cloudlog.core_test/timeline!0" "bob" ["alice" "bob"])]))
+"The `:name` component in the produced events is derived from the name of the rule, a 'bang' (!) and
+the index of the link that emitted this event in the overall rule.  An emitter always represents
+the first link in a rule, so this value is always 0."
 
 [[:chapter {:title "multiplier: Create a Function Applying Rules to Facts"}]]
 "The lowest level of event processing is taking two corresponding events 
